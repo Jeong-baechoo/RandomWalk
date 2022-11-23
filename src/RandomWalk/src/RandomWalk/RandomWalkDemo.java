babb1dfdaba82af370c3fe7fa20862ceb9fdce3f
Jeong yong hwan <baechoo0905@naver.com>
2022-11-24 01:24:12 +0900

    Revert "Delete src/RandomWalk directory"
    This reverts commit d0fde0adbca8b50266176e858f06efdf1ef8b6e6.

---
+++ src/RandomWalk/src/RandomWalk/RandomWalkDemo.java
@@ -0,0 +1,43 @@
+package RandomWalk;
+
+import java.util.Arrays;
+
+public class RandomWalkDemo{
+	static int[][] field = new int[10][10];
+	static int[][] fulledField = new int[10][10]; // 완성 
+	public static void main(String[] args) {
+		
+		for (int i = 0; i < field.length; i++) {
+			for (int j = 0; j < field.length; j++) {
+				field[i][j] = 0;
+				fulledField[i][j] = 1;
+			}
+		}
+		int count = 0;
+		
+//		LadyBug bug1 = new LadyBug();
+//		while(!Arrays.deepEquals(field, fulledField)) { 
+//			Movement.move(bug1);
+//			count ++;
+//		}
+		
+//		for (int i = 0; i < field.length; i++) {
+//			for (int j = 0; j < field.length; j++) {
+//				System.out.print(field[i][j]);
+//			}
+//			System.out.println();
+//		}
+		
+		System.out.println("소요시간: " + count);
+	}
+	
+	static void initField()
+	{
+		for (int i = 0; i < field.length; i++) {
+			for (int j = 0; j < field.length; j++) {
+				field[i][j] = 0;
+				fulledField[i][j] = 1;
+			}
+		}
+	}
+}
